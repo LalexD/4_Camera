899be7eb5c5ec3a680d88d8118fbf727b6c4c604
LalexD <LalexD@mail.ru>
2014-06-30 17:45:23 +0400

    no message

--- gen/com/lad/camera/R.java
+++ gen/com/lad/camera/R.java
@@ -26,19 +26,18 @@ public final class R {
     }
     public static final class id {
         public static final int ViewPhoto=0x7f080000;
-        public static final int btnPhoto=0x7f080001;
-        public static final int name_file=0x7f080002;
-        public static final int save_photo=0x7f080003;
+        public static final int photoButton=0x7f080001;
+        public static final int save_photo=0x7f080002;
     }
     public static final class layout {
         public static final int camera_layout=0x7f030000;
-        public static final int dialog_save_photo=0x7f030001;
-        public static final int fragment_camera_main=0x7f030002;
+        public static final int fragment_camera_main=0x7f030001;
     }
     public static final class menu {
         public static final int camera_main=0x7f070000;
     }
     public static final class string {
+        public static final int ViewPhoto=0x7f05000a;
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int cancel_dialog=0x7f050006;
@@ -46,6 +45,7 @@ public final class R {
         public static final int message_save_dialog=0x7f050009;
         public static final int name_file=0x7f050005;
         public static final int photo=0x7f050003;
+        public static final int photo_button=0x7f05000b;
         public static final int save=0x7f050004;
         public static final int save_dialog=0x7f050007;
         public static final int title_save_dialog=0x7f050008;
